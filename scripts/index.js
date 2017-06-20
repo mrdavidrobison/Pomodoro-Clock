5a7a48604803102e33a3d7018d604904cdb83574
david robison <mrdavidrobison@gmail.com>
2017-06-20 11:06:21 -0700

    add minus add click functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,29 @@
 $(document).ready(function(){
   var buzzer = $("#buzzer")[0];
-  var count = parseInt($("minus5Clock").html());
-  
+  var sessionCount = parseInt($("numS").html());
+  var breakCount = parseInt($("numB").html());
+
+  $("#minusS").click(function(){
+    if (sessionCount > 5){
+      sessionCount -= 5;
+      $("#numS").html(sessionCount);
+    }
+  })
+
+  $("#addS").click(function(){
+    sessionCount += 5;
+    $("#numS").html(sessionCount);
+  })
+
+  $("#minusB").click(function(){
+    if (breakCount > 5){
+      breakCount -= 5;
+      $("#numB").html(breakCount);
+    }
+  })
+
+  $("#addB").click(function(){
+    breakCount += 5;
+    $("#numB").html(breakCount);    
+  })
 });
