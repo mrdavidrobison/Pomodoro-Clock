639540cbfb92e9643f8ef756cb1ae63077f82e42
david robison <mrdavidrobison@gmail.com>
2017-06-21 11:48:58 -0700

    add start function

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,55 @@ $(document).ready(function(){
   var sessionCount = parseInt($("numS").html());
   var breakCount = parseInt($("numB").html());
 
+$("#reset").hide();
+  $("#start").click(function() {
+    var counter = setInterval(timer, 1000);
+    count *= 60;
+    function timer() {
+      //Hide variables
+      $(
+        "#start, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #breakNum, #title1, #title2"
+      ).hide();
+      $("#timeType").show();
+      $("#timeType").html("Session Time: ");
+      count -= 1;
+      if (count === 0) {
+        buzzer.play();
+        clearInterval(counter);
+        var startBreak = setInterval(breakTimer, 1000);
+        $("#num").hide();
+      }
+      if (count % 60 >= 10) {
+        $("#num").html(Math.floor(count / 60) + ":" + count % 60);
+      } else {
+        $("#num").html(Math.floor(count / 60) + ":" + "0" + count % 60);
+      }
+
+      function breakTimer() {
+        $("#timeType").html("Break Time: ");
+        $("#breakNum").show();
+        breakTime *= 60;
+        $("#timeType").show();
+        breakTime -= 1;
+        if (breakTime === 0) {
+          clearInterval(startBreak);
+          buzzer.play();
+          $("#reset").show();
+          $("#breakNum, #timeType").hide();
+        }
+        if (breakTime % 60 >= 10) {
+          $("#breakNum").html(
+            Math.floor(breakTime / 60) + ":" + breakTime % 60
+          );
+        } else {
+          $("#breakNum").html(
+            Math.floor(breakTime / 60) + ":" + "0" + breakTime % 60
+          );
+        }
+      }
+    }
+  });
+
   $("#minusS").click(function(){
     if (sessionCount > 5){
       sessionCount -= 5;
