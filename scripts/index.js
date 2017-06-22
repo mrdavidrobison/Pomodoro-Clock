3c1bf384e42a62f29bf4bc0294f8375fd3196a2b
david robison <mrdavidrobison@gmail.com>
2017-06-22 13:57:19 -0700

    functions

--- scripts/index.js
+++ scripts/index.js
@@ -1,35 +1,57 @@
 $(document).ready(function(){
-  var buzzer = $("#buzzer")[0];
-  var sessionCount = parseInt($("numS").html());
-  var breakCount = parseInt($("numB").html());
 
-$("#reset").hide();
-  $("#start").click(function() {
+  var buzzer = $("#buzzer")[0];
+  var countS = parseInt($("#numS").html());
+  var countB = parseInt($("#numB").html());
+
+  // $("#reset").hide();
+
+  $("#minusS").click(function(){
+    if (countS > 5) {
+      countS -= 5;
+    };
+    $("#numS").html(countS);
+  })
+
+  $("#addS").click(function(){
+    countS += 5;
+    $("#numS").html(countS);
+  })
+
+  $("#minusB").click(function(){
+    if (countB > 5){
+      countB -= 5;
+    }
+    $("#numB").html(countB);
+  })
+
+  $("#addB").click(function(){
+    countB += 5;
+    $("#numB").html(countB);
+  })
+
+  $("#start").click(function(){
     var counter = setInterval(timer, 1000);
-    count *= 60;
-    function timer() {
-      //Hide variables
-      $(
-        "#start, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #breakNum, #title1, #title2"
-      ).hide();
+    countS *= 60;
+    function timer(){
+      $("#addS, #minusS, #numS, #titleS, #addB, #minusB, #numB, #titleB").hide();
       $("#timeType").show();
       $("#timeType").html("Session Time: ");
-      count -= 1;
-      if (count === 0) {
+      countS -= 1;
+      if (countS === 0){
         buzzer.play();
         clearInterval(counter);
         var startBreak = setInterval(breakTimer, 1000);
-        $("#num").hide();
+        $("#numS").hide();
       }
-      if (count % 60 >= 10) {
-        $("#num").html(Math.floor(count / 60) + ":" + count % 60);
+      if (countS % 60 >= 10) {
+        $("#numS").html(Math.floor(countS / 60) + ":" + countS % 60);
       } else {
-        $("#num").html(Math.floor(count / 60) + ":" + "0" + count % 60);
-      }
-
+        $("#numS").html(Math.floor(countS / 60) + ":" + "0" + countS % 60);
+      }  
       function breakTimer() {
         $("#timeType").html("Break Time: ");
-        $("#breakNum").show();
+        $("#numB").show();
         breakTime *= 60;
         $("#timeType").show();
         breakTime -= 1;
@@ -37,14 +59,14 @@ $("#reset").hide();
           clearInterval(startBreak);
           buzzer.play();
           $("#reset").show();
-          $("#breakNum, #timeType").hide();
+          $("#numB, #timeType").hide();
         }
         if (breakTime % 60 >= 10) {
-          $("#breakNum").html(
+          $("#numB").html(
             Math.floor(breakTime / 60) + ":" + breakTime % 60
           );
         } else {
-          $("#breakNum").html(
+          $("#numB").html(
             Math.floor(breakTime / 60) + ":" + "0" + breakTime % 60
           );
         }
@@ -52,39 +74,15 @@ $("#reset").hide();
     }
   });
 
-  $("#reset").click(function() {
-    console.log(4444444);
-    count = 25;
-    breakTime = 25;
+  $("#reset").click(function(){
+    
+    count=25;
+    breakTime=25;
+
     $("#num").html(count);
     $("#breakNum").html(breakTime);
-    $(
-      "#start, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #breakNum, #num, #title1, #title2"
-    ).show();
+    $("#start, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #breakNum, #num, #title1, #title2").show();
     $("#reset, #timeType").hide();
+
   });
-
-  $("#minusS").click(function(){
-    if (sessionCount > 5){
-      sessionCount -= 5;
-      $("#numS").html(sessionCount);
-    }
-  })
-
-  $("#addS").click(function(){
-    sessionCount += 5;
-    $("#numS").html(sessionCount);
-  })
-
-  $("#minusB").click(function(){
-    if (breakCount > 5){
-      breakCount -= 5;
-      $("#numB").html(breakCount);
-    }
-  })
-
-  $("#addB").click(function(){
-    breakCount += 5;
-    $("#numB").html(breakCount);    
-  })
-});
+})
